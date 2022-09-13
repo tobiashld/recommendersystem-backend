d659122a1d8b07efa27f417b37c4598429d2098d
tobiashld <tobias02173@gmail.com>
2022-09-13 21:09:33 +0200

    docker changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
