f245e68cbb8f595a9112a8d293cac2240a92849f
tobiashld <tobias02173@gmail.com>
2022-09-14 18:32:53 +0200

    gunicorn hinzugefügt

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,9 @@ RUN apt-get update && apt-get -y install python3 python3-pip
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
+COPY . /app
 WORKDIR /app
-COPY app/ .
 
-ENTRYPOINT ["python3", "main.py"]
+
+
+ENTRYPOINT ["./gunicorn.sh"]
--- app/main.py
+++ app/main.py
@@ -1,28 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask,jsonify
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
 api =   Api(app)
 
-class HelloWorld(Resource):
-    """
-    Test API Endpoint
-    """
-    def get(self):
-        """
-        Getter for hello world
-        """
-
-        data={"data": "Hi there, Programming Project!"}
-
-        return data
-
-api.add_resource(HelloWorld,'/hello')
+@app.route('/', methods = ['GET'])
+def mainRoute():
+    return jsonify(hello="world")
 
 
 if __name__=='__main__':
-    cfg_port = os.getenv('PORT', "5000")
+    cfg_port = os.getenv('PORT', "80")
     app.run(host="0.0.0.0", port=cfg_port)#, debug=True)
     #Test
