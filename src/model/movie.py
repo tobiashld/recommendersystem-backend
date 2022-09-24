532b17523417235f335f3ec7c0a871723f6d3c45
marharms <mareike.harms@hs-osnabrueck.de>
2022-09-24 12:28:11 +0200

    PRS-25_objektorientiertes Model geloescht

--- src/model/movie.py
+++
@@ -1,9 +0,0 @@
-import string
-
-class movie:
-    id: int
-    title: string    
-    beschreibung: string
-    releaseJahr: int
-    imgPath: string
-   #userGivenRating: int
--- src/model/movie_recommendations.py
+++
@@ -1,6 +0,0 @@
-from src.model.movie import movie
-
-
-class movie_recommendations:
-    requested_movie: movie
-    recommendations: movie
