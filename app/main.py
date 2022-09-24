28ef2dc6b7000b5dee527579c7408a84312bd060
tobiashld <tobias02173@gmail.com>
2022-09-24 03:37:43 +0200

    idk 3.0

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask,jsonify,request,abort
 from flask_restful import Api, Resource
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 import requests
 
 app = Flask(__name__)
