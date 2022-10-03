b044a2233561f81810fe9bad8caab91e5c4ea881
marharms <mareike.harms@hs-osnabrueck.de>
2022-10-03 16:48:27 +0200

    PRS-25 Listenverarbeitung 2.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from src.service.solrservice import search_film_by_name
-from src.service.recommenderservice import recommend_for_movie as movieservice,recommend_for_movies as sammlungmovieservice
+from src.service.recommenderservice import recommend_for_movie as movieservice, recommend_for_movies as sammlungmovieservice, recommend_for_movie_list as listmovieservice
 from flask_restful import Api, Resource
 from flask import Flask,jsonify,request,abort
 from flask_cors import CORS,cross_origin
@@ -37,6 +37,11 @@ def getSammlung():
     ids = request.args.get("ids").split(" ")
     return {"result":sammlungmovieservice(ids)}
 
+@app.route('/get/Geteiltenachbarn', methods = ['GET'])
+def getGeteiltenachbarn():
+    print(request.args.get("ids"))
+    ids = request.args.get("ids").split(" ")
+    return {"result":listmovieservice(ids)}
 
 @app.route('/dropdownsearch', methods = ['GET'])
 @cross_origin()
